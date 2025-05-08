2fbda2069042c863f896030b7b0cca228f2f06d1
pounceinwindow <140322078+pounceinwindow@users.noreply.github.com>
2025-05-09 00:01:04 +0300

    w

--- alg/alg/PigeonholeSort.cs
+++ alg/alg/PigeonholeSort.cs
@@ -2,19 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 
-/// <summary>
 /// Класс для сортировки Pigeonhole Sort и подсчёта итераций.
-/// </summary>
 public class PigeonholeSort
 {
-    /// <summary>
     /// Считает общее число операций (итераций) внутри алгоритма.
-    /// </summary>
     public int Iterations { get; private set; }
 
-    /// <summary>
-    /// Сортирует массив методом "голубиных нор".
-    /// </summary>
     public void Sort(int[] array)
     {
         Iterations = 0;
@@ -25,7 +18,6 @@ public class PigeonholeSort
         int max = array.Max();
         int range = max - min + 1;
 
-        // Создаём "норки"
         List<int>[] holes = new List<int>[range];
         for (int i = 0; i < range; i++)
             holes[i] = new List<int>();
@@ -37,7 +29,6 @@ public class PigeonholeSort
             Iterations++;
         }
 
-        // Сбор обратно в массив
         int idx = 0;
         for (int i = 0; i < range; i++)
         {
